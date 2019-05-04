a8a0a8464d91cfcfb04d761c312dff60628320f0
Thinh Phan <thinh.phan@sony.com>
2019-05-04 08:05:53 -0500

    Add blueprintjs

--- components/header.js
+++
@@ -1,9 +0,0 @@
-function Header() {
-  return (
-    <header>
-      <h1>My site is under construction!</h1>
-    </header>
-  )
-}
-
-export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,33 @@
-import Link from 'next/link'
-import Header from '../components/header'
+import 'normalize.css/normalize.css'
+import '@blueprintjs/core/lib/css/blueprint.css'
+import '@blueprintjs/icons/lib/css/blueprint-icons.css'
+
+import {
+  Navbar,
+  NavbarGroup,
+  NavbarHeading,
+  NavbarDivider,
+  Button,
+  Classes,
+  Alignment
+} from '@blueprintjs/core'
 
 function Index() {
   return (
-    <main>
-      <Header />
-      <section>
-        <Link href='/about'>
-          <a>Go to About Thinh Phan</a>
-        </Link>
-      </section>
-    </main>
+    <div className='bp3-dark'>
+      <Navbar>
+        <NavbarGroup align={Alignment.LEFT}>
+          <NavbarHeading>thinhphan.dev</NavbarHeading>
+          <NavbarDivider />
+          <Button className={Classes.MINIMAL} icon='home' text='Home' />
+          <Button className={Classes.MINIMAL} icon='document' text='Files' />
+        </NavbarGroup>
+      </Navbar>
+
+      <main>
+        <section>The site is under construction</section>
+      </main>
+    </div>
   )
 }
 
