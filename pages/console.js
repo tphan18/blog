2756db5fb229d7b99c3e18d4d245667729cbbc9d
Thinh Phan <thinh.phan@sony.com>
2019-05-20 18:21:39 -0500

    Add console

--- pages/console.js
+++ pages/console.js
@@ -1,7 +1,59 @@
-import { H4 } from '@blueprintjs/core'
+import React, { Component } from 'react'
+import { Card } from '@blueprintjs/core'
+import { Terminal } from 'xterm'
+import 'xterm/dist/xterm.css'
 
-function Console() {
-  return <H4>The site is console</H4>
+export default class Console extends Component {
+  constructor(props) {
+    super(props)
+    this.terminalRef = React.createRef()
+  }
+
+  componentDidMount() {
+    this.terminal = new Terminal()
+    this.terminal.open(this.terminalRef.current)
+
+    function runFakeTerminal(term) {
+      if (term._initialized) {
+        return
+      }
+
+      term._initialized = true
+
+      term.prompt = () => {
+        term.write('\r\n$ ')
+      }
+
+      term.writeln('Welcome to my server')
+      term.writeln('Type some keys and commands to play around.')
+      term.writeln('')
+      term.prompt()
+
+      term.on('key', function(key, ev) {
+        const printable =
+          !ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey
+
+        if (ev.keyCode === 13) {
+          term.prompt()
+        } else if (ev.keyCode === 8) {
+          // Do not delete the prompt
+          if (term._core.buffer.x > 2) {
+            term.write('\b \b')
+          }
+        } else if (printable) {
+          term.write(key)
+        }
+      })
+
+      term.on('paste', function(data) {
+        term.write(data)
+      })
+    }
+
+    runFakeTerminal(this.terminal)
+  }
+
+  render() {
+    return <div ref={this.terminalRef} />
+  }
 }
-
-export default Console
